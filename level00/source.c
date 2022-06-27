11969c84a093fbc484f81cd864d21418eda050d6
Francois THIERREE <frthierr@e1r8p11.42.fr>
2022-06-27 15:23:04 +0200

    added C source code for level00

--- level00/source.c
+++ level00/source.c
@@ -1,3 +1,23 @@
-int main() {
-    
+int main()
+{
+    int password;
+    int local[3];
+
+    puts("***********************************");
+    puts("* \t     -Level00 -\t\t  *");
+    puts("***********************************");
+    printf("Password:");
+    scanf("%d", &password);
+
+    if (password == 0x149c)
+    {
+        puts("\nAuthenticated!");
+        system("/bin/sh");
+    }
+    else
+    {
+        puts("\nInvalid Password!");
+    }
+
+    return 0;
 }
