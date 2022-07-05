d2643857c4763c145a3be01b29a8d7597cc8130e
Pierre TRICHET <pitriche@e1r4p9.42.fr>
2022-07-05 12:12:29 +0200

    cleaned level04/source.c

--- level04/source.c
+++ level04/source.c
@@ -6,38 +6,45 @@
 #include <signal.h>
 #include <sys/ptrace.h>
 
-int main() {
-    pid_t   pid; // +0xac
-    uint    val; // 	+0xa8
-	uint	val2; //	+0xa4
-	uint	val3; //	+0xa0
-    char    buffer[0x20]; // +0x20
-    uint   	w_status; // 	+0x1c
+typedef unsigned int uint;
 
-    pid = fork();
+int	main(void)
+{
+	pid_t	pid;			//	+0xac
+	uint	val;			//	+0xa8
+	uint	val2;			//	+0xa4
+	uint	val3;			//	+0xa0
+	char	buffer[0x20];	//	+0x20
+	uint	w_status;		//	+0x1c
 
-    bzero(buffer, 0x20);
+	pid = fork();
 
-    val = 0;
-    w_status = 0;
+	bzero(buffer, 0x20);
 
-    if (pid == 0) {
-        prctl(PR_SET_PDEATHSIG, 1);
-		ptrace(0, 0, 0 ,0);
+	val = 0;
+	w_status = 0;
+
+	if (pid == 0)
+	{
+		prctl(PR_SET_PDEATHSIG, 1);
+		ptrace(0, 0, 0, 0);
 		puts("Give me some shellcode, k");
 		gets(buffer);
-    }
-	while (true) {
-	  	wait(&w_status);
-		if (WIFEXITED(w_status) || WIFEXITED((w_status + 1) / 2)) {
+	}
+	while (true)
+	{
+		wait(&w_status);
+		if (WIFEXITED(w_status) || WIFEXITED((w_status + 1) / 2))
+		{
 			puts("child is exiting...");
 			break;
-		} else if (ptrace(0x3, pid, 0x2c, 0) == 0xb) {
+		}
+		else if (ptrace(0x3, pid, 0x2c, 0) == 0xb)
+		{
 			puts("no exec() for you");
 			kill(pid, SIGKILL);
 			break;
 		}
 	}
-
-	return 0;
+	return (0);
 }
