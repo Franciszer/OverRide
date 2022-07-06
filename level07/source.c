631e095db1a68933150080a52f39175712d68e70
Pierre TRICHET <pitriche@e1r8p10.42.fr>
2022-07-06 18:54:41 +0200

    halfway done level07/source.c

--- level07/source.c
+++ level07/source.c
@@ -3,27 +3,59 @@
 typedef unsigned int	u32;
 typedef unsigned char	u8;
 
+u32	store_number(char *buf)
+{
+	return (0);
+}
+
+u32	read_number(char *buf)
+{
+	return (0);
+}
+
 int	main(int ac, char **argv, char **argenv)
 {
 	u32		canary;		/* esp+0x1cc */
-	char	buf2[24];	/* esp+0x1b4 */
+	char	cmd[20];	/* esp+0x1b8 */
+	u32		var;		/* esp+0x1b4 */
 						u8	pad[300];
 	char	buf[100];	/* esp+0x024 */
-						u8	pad[8];
-	char	**av;		/* esp+0x01c */
-	char	**env;		/* esp+0x018 */
 
-	av = argv;
-	env = argenv;
 	canary = 20;
-	bzero(buf2, 24);
+	var = 0;
+	bzero(cmd, 20);
 	bzero(buf, 100);
-	do
+	for (char **av = argv; *av; ++av)
+		bzero(*av, strlen(*av));
+	for (char **env = argenv; *env; ++env)
+		bzero(*env, strlen(*env));
+	puts(
+	"----------------------------------------------------\n"
+	"  Welcome to wil's crappy number storage service!   \n"
+	"----------------------------------------------------\n"
+	" Commands:                                          \n"
+	"    store - store a number into the data storage    \n"
+	"    read  - read a number from the data storage     \n"
+	"    quit  - exit the program                        \n"
+	"----------------------------------------------------\n"
+	"   wil has reserved some storage :>                 \n"
+	"----------------------------------------------------\n\n");
+	for (;;)
 	{
-
-	} while (*av);
-
-
+		printf("Input command: ");
+		fgets(cmd + 4, 20, stdin);
+		cmd[strlen(cmd) - 1] = 0;
+		if (strncpy(cmd, "store", 5) == 0)
+			var = store_number(buf);
+		if (strncpy(cmd, "read", 4) == 0)
+			var = read_number(buf);
+		if (strncpy(cmd, "quit", 4) == 0)
+			break ;
+		if (var == 0)
+			printf(" Completed %s command successfully\n", cmd);
+		else
+			printf(" Failed to do %s command\n", cmd);
+	}
 	if (canary != 20)
 		*(int *)0 = 0;	/* __stack_chk_fail, induce crash if canary smashed */
 	return (0);
